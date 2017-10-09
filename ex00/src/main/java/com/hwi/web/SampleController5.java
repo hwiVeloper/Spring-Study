3d54530bd39033ebd4edd06e61c22d5afc95a28c
hwiVeloper <zziller03@gmail.com>
2017-10-10 01:25:24 +0900

    Spring MVC Controller 연습

---
+++ ex00/src/main/java/com/hwi/web/SampleController5.java
@@ -0,0 +1,17 @@
+package com.hwi.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hwi.domain.ProductVO;
+
+@Controller
+public class SampleController5 {
+	@RequestMapping("/doJSON")
+	public @ResponseBody ProductVO doJSON() {
+		ProductVO vo = new ProductVO("샘플상품", 3000);
+		
+		return vo;
+	}
+}
