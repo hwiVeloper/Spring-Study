e763aba4c3e0d6eb27b2b87eac9f48a2e90ada79
hwiVeloper <zziller03@gmail.com>
2017-10-17 23:32:34 +0900

    공부내용 전수

--- ex00/src/test/java/com/hwi/web/MyBatisTest.java
+++ ex00/src/test/java/com/hwi/web/MyBatisTest.java
@@ -15,13 +15,19 @@ public class MyBatisTest {
 	@Inject
 	private SqlSessionFactory sqlFactory;
 	
+	// pom.xml 라이브러리만 가지고오고
+	// root-context.xml 가지고 온 라이브러리를 설정
+	// 해당 소스 : 실제로 라이브러리를 사용 (Inject Annotation 사용)
+	
 	@Test
 	public void testFactory() {
+		// SQL Factory Test
 		System.out.println(sqlFactory);
 	}
 	
 	@Test
 	public void testSession() throws Exception {
+		// SQL Factory - Session Test
 		try {
 			SqlSession session = sqlFactory.openSession();
 			
