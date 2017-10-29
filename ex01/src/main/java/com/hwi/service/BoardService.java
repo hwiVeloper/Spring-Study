7cc24084f641a1dbdfb6380ea9472ad52d3bbf9d
hwiVeloper <zziller03@gmail.com>
2017-10-29 23:11:11 +0900

    게시판 리스트 및 등록

---
+++ ex01/src/main/java/com/hwi/service/BoardService.java
@@ -0,0 +1,17 @@
+package com.hwi.service;
+
+import java.util.List;
+
+import com.hwi.domain.BoardVO;
+
+public interface BoardService {
+	public void regist(BoardVO board) throws Exception;
+	
+	public BoardVO read(Integer bno) throws Exception;
+	
+	public void modify(BoardVO board) throws Exception;
+	
+	public void remove(Integer bno) throws Exception;
+	
+	public List<BoardVO> listAll() throws Exception;
+}
---
+++ ex01/src/main/webapp/WEB-INF/views/board/listAll.jsp
@@ -0,0 +1,9 @@
+<table class="table table-bordered">
+	<tr>
+		<th style="width: 10px">BNO</th>
+		<th>TITLE</th>
+		<th>WRITER</th>
+		<th>REGDATE</th>
+		<th style="width: 40px">VIEWCNT</th>
+	</tr>
+</table>
