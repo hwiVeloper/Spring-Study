6fd204dd3d0e8d9f6acb8c602444c60af66f09e3
hwiVeloper <zziller03@gmail.com>
2017-12-31 19:06:21 +0900

    AOP 실습하기

--- ex03/src/main/java/com/hwi/service/MessageServiceImpl.java
+++ ex03/src/main/java/com/hwi/service/MessageServiceImpl.java
@@ -3,6 +3,7 @@ package com.hwi.service;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hwi.domain.MessageVO;
 import com.hwi.persistence.MessageDAO;
@@ -17,6 +18,7 @@ public class MessageServiceImpl implements MessageService {
 	@Inject
 	private PointDAO pointDAO;
 
+	@Transactional
 	@Override
 	public void addMessage(MessageVO vo) throws Exception {
 		messageDAO.create(vo);
